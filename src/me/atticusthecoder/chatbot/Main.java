ab65fbb564e0e1db3c6332b086e242f7c5a6dcf8
Atticus Zambrana <angusbeefsteak14@gmail.com>
2019-08-16 10:12:17 -0700

    Added all code from workspace

---
+++ src/me/atticusthecoder/chatbot/Main.java
@@ -0,0 +1,12 @@
+package me.atticusthecoder.chatbot;
+
+public class Main {
+	/*
+	 * Atticus Chatbot
+	 * Written by Atticus Zambrana
+	 */
+	
+	public static void main(String[] args) {
+		//TODO: Maybe later in the programs lifetime, we could use the args for something usful
+	}
+}
